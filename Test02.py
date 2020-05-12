01d979a479bb8f5f69be7fd92ae4815d5cc0c99b
Pjus <wnstjd117@naver.com>
2020-05-12 16:35:17 +0900

    class test

--- Test02.py
+++ Test02.py
@@ -1,1 +1,13 @@
-import Test
+import filename
+
+# Jane, Jane + fff
+a = filename.classname()
+print(a.func1()) 
+print(a.func2())
+
+# Holy
+b = filename.classname('Holy')
+print(b.func1())
+
+# None
+print(filename.classname.__init__(a,'Holy'))
---
+++ filename.py
@@ -0,0 +1,8 @@
+class classname:
+    def __init__(self, value = 'Jane'):
+        self.thing = value
+        self.thing2 = value + ' fff'
+    def func1(self):
+        return self.thing
+    def func2(self):
+        return self.thing2
