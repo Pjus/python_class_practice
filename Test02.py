a111de64df9d7444747d30f8f5d89b233db024c4
Pjus <wnstjd117@naver.com>
2020-05-12 17:19:58 +0900

    class practice

--- Test02.py
+++ Test02.py
@@ -1,13 +1,22 @@
 import filename
 
-# Jane, Jane + fff
-a = filename.classname()
-print(a.func1()) 
-print(a.func2())
+# # Jane, Jane + fff
+# a = filename.classname()
+# print(a.func1()) 
+# print(a.func2())
 
-# Holy
-b = filename.classname('Holy')
-print(b.func1())
+# # Holy
+# b = filename.classname('Holy')
+# print(b.func1())
 
-# None
-print(filename.classname.__init__(a,'Holy'))
+# # None
+# print(filename.classname.__init__(a,'Holy'))
+
+
+pt1 = filename.Point()
+print(pt1.x, pt1.y)
+
+pt2 = filename.Point(3, 5)
+print(pt2.x, pt2.y)
+
+pt2.innerPrint('dsdsd :')
--- filename.py
+++ filename.py
@@ -1,4 +1,4 @@
-class classname:
+class classname():
     def __init__(self, value = 'Jane'):
         self.thing = value
         self.thing2 = value + ' fff'
@@ -6,3 +6,11 @@ class classname:
         return self.thing
     def func2(self):
         return self.thing2
+
+
+class Point():
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    def innerPrint(self, infor):
+        print(infor, self.x, self.y)
