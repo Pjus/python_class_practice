ab304fb101db5037735285cd0c68ca5c15ae6dda
Pjus <wnstjd117@naver.com>
2020-05-13 17:48:31 +0900

    update

--- backtrader/bactrader_ex.py
+++ backtrader/bactrader_ex.py
@@ -1,4 +1,6 @@
 import backtrader as bt
+import os
+
 
 """
 class PandasData(feed.DataBase):
@@ -30,10 +32,10 @@ class PandasData(feed.DataBase):
     
 """
 
-class MyStrategy(bt.Strategy):
+class MyStrategy_1(bt.Strategy):
 
     def __init__(self):  # Initiation
-        self.sma = btind.SimpleMovingAverage(period=15)  # Processing
+        self.sma = bt.ind.SimpleMovingAverage(period=15)  # Processing
 
     def next(self):  # Processing
         if self.sma > self.data.close:
@@ -51,20 +53,67 @@ class SmaCross(bt.SignalStrategy):
         self.signal_add(bt.SIGNAL_LONG, crossover)
 
 
+class DonchianChannels(bt.Indicator):
+    '''
+    Params Note:
+      - `lookback` (default: -1)
+        If `-1`, the bars to consider will start 1 bar in the past and the
+        current high/low may break through the channel.
+        If `0`, the current prices will be considered for the Donchian
+        Channel. This means that the price will **NEVER** break through the
+        upper/lower channel bands.
+    '''
+
+    alias = ('DCH', 'DonchianChannel',)
+
+    lines = ('dcm', 'dch', 'dcl',)  # dc middle, dc high, dc low
+    params = dict(
+        period=20,
+        lookback=-1,  # consider current bar or not
+    )
+
+    plotinfo = dict(subplot=False)  # plot along with data
+    plotlines = dict(
+        dcm=dict(ls='--'),  # dashed line
+        dch=dict(_samecolor=True),  # use same color as prev line (dcm)
+        dcl=dict(_samecolor=True),  # use same color as prev line (dch)
+    )
+
+    def __init__(self):
+        hi, lo = self.data.high, self.data.low
+        if self.p.lookback:  # move backwards as needed
+            hi, lo = hi(self.p.lookback), lo(self.p.lookback)
+
+        self.l.dch = bt.ind.Highest(hi, period=self.p.period)
+        self.l.dcl = bt.ind.Lowest(lo, period=self.p.period)
+        self.l.dcm = (self.l.dch + self.l.dcl) / 2.0  # avg of the above
+
+
+class MyStrategy(bt.Strategy):
+    def __init__(self):
+        self.myind = DonchianChannels()
+
+    def next(self):
+        if self.data[0] > self.myind.dch[0]:
+            self.buy()
+        elif self.data[0] < self.myind.dcl[0]:
+            self.sell()
+
+
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
     
-    cerebro.broker.setcash(1337.0)
+    cerebro.broker.setcash(1000.0)
     cerebro.broker.setcommission(commission=0.001)
 
+    # print(os.getcwd())
 
-
-    data = bt.feeds.YahooFinanceCSVData(dataname='AAPL.csv')
+    data = bt.feeds.YahooFinanceCSVData(dataname='./backtrader/AAPL.csv')
     cerebro.adddata(data)
-    cerebro.addstrategy(SmaCross)
-    cerebro.run()
+    cerebro.addstrategy(MyStrategy)
 
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    cerebro.run()
     
     print('Ending Portfolio Value: %.2f' % cerebro.broker.getvalue())
     cerebro.plot()
--- predict.py
+++ predict.py
@@ -9,11 +9,6 @@ pre3 = cla.predict3()
 
 pre4 = cla.predict4()
 
-emp1 = pre1.getPredict(3)
-emp2 = pre2.getPredict(3)
-emp3 = pre3.getPredict(3, 5)
-emp4 = pre4.getPredict(7, 9)
-
 pickle.dump(pre1, open('./pre1.pkl', 'wb'))
 pickle.dump(pre2, open('./pre2.pkl', 'wb'))
 pickle.dump(pre3, open('./pre3.pkl', 'wb'))
@@ -25,7 +20,7 @@ emp_pkl2 = pickle.load(open('./pre2.pkl', 'rb'))
 emp_pkl3 = pickle.load(open('./pre3.pkl', 'rb'))
 emp_pkl4 = pickle.load(open('./pre4.pkl', 'rb'))
 
-emp_pkl1.getPredict(3)
-emp_pkl2.getPredict(3)
+emp_pkl1.getPredict(7)
+emp_pkl2.getPredict(7)
 emp_pkl3.getPredict(7, 9)
 emp_pkl4.getPredict(7, 9)
